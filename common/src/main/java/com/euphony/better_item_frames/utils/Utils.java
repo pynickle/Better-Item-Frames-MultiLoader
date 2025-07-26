c63c94845a56bb93f9a5700f34f901181da70719
ENC_Euphony <2330458484@qq.com>
2025-07-26 15:11:11 +0800

    1.3

--- common/src/main/java/com/euphony/better_item_frames/utils/Utils.java
+++ common/src/main/java/com/euphony/better_item_frames/utils/Utils.java
@@ -1,6 +1,7 @@
 package com.euphony.better_item_frames.utils;
 
 import com.euphony.better_item_frames.api.ICustomItemFrame;
+import com.euphony.better_item_frames.config.BIFConfig;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.decoration.ItemFrame;
@@ -20,12 +21,20 @@ public class Utils {
     }
 
     public static void setInvisibility(AbstractThrownPotion thrownPotion, boolean invisibility) {
-        AABB checkBox = thrownPotion.getBoundingBox().inflate(2.0D, 1.0D, 2.0D);
+        AABB checkBox = getBoundingBox(thrownPotion);
         List<ItemFrame> itemFrames = thrownPotion.level().getEntitiesOfClass(ItemFrame.class, checkBox);
         for (ItemFrame frame : itemFrames) {
             ICustomItemFrame itemFrame = (ICustomItemFrame) frame;
             if (invisibility != itemFrame.better_item_frames$getIsInvisible())
                 itemFrame.better_item_frames$setIsInvisible(invisibility);
+        }
+    }
+
+    private static AABB getBoundingBox(AbstractThrownPotion thrownPotion) {
+        if(BIFConfig.getInstance().getSplashPotionRange() == BIFConfig.SplashPotionRange.HALF) {
+            return thrownPotion.getBoundingBox().inflate(2.0D, 1.0D, 2.0D);
+        } else {
+            return thrownPotion.getBoundingBox().inflate(4.0D, 2.0D, 4.0D);
         }
     }
 }
